6ff7ccc4920288ac4598bd1c140a1cdff86d9a8e
mervydev <65782957+mervydevmedia@users.noreply.github.com>
2022-06-02 22:05:46 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,19 +2,26 @@
 const montant = document.getElementById('montant')
 const equivalance = document.getElementById('equivalance')
 const divEquivalance = document.querySelector('.equivalance')
+const montantRub = document.getElementById('montant-rub')
+const equivalanceRub = document.getElementById('equivalance-rub')
+const divEquivalanceRub = document.querySelector('.equivalance-rub')
+
+console.log(divEquivalance);
 
 // EVENT LISTENER
 montant.addEventListener('input', conversion)
+montantRub.addEventListener('input', conversionRub)
 
 // Init Rate
 let rate = 0.102
+let rateRub = 8.33333333
 
 function conversion () {
   // Get user input value
   const userInputValue = montant.value
+  console.log(userInputValue);
   // Conersion
   const tatalMoney = new Intl.NumberFormat().format(userInputValue * rate)
-
   // Insert to HTML
   equivalance.innerHTML = `<b>${tatalMoney}</b>`
 
@@ -23,5 +30,21 @@ function conversion () {
     divEquivalance.style.display = 'block'
   } else {
     divEquivalance.style.display = 'none'
+  }
+}
+
+function conversionRub() {
+  // Get user input value
+  const userInputVal = montantRub.value
+  // Conersion
+  const tatalMoney = new Intl.NumberFormat().format(userInputVal * rateRub)
+  // Insert to HTML
+  equivalanceRub.innerHTML = `<b>${tatalMoney}</b>`
+
+  // Dynamic show div equivalance
+  if (userInputVal) {
+    divEquivalanceRub.style.display = 'block'
+  } else {
+    divEquivalanceRub.style.display = 'none'
   }
 }
