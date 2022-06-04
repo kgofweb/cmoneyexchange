d60314eb501ecc7a83085c6be1fc6ce82d7c6ba4
mervydev <65782957+mervydevmedia@users.noreply.github.com>
2022-06-04 13:46:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ montant.addEventListener('input', conversion)
 montantRub.addEventListener('input', conversionRub)
 
 // Init Rate
-let rate = 0.10141
+let rate = 0.1025
 let rateRub = 8.69565217
 
 function conversion () {
