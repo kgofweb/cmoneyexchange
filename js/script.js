b7455be5e5853e5289f16ba1d383e102ac7071fa
mervydevmedia <65782957+kgofweb@users.noreply.github.com>
2022-07-11 15:53:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
 // DOM HANDLE
-const title = document.querySelector('.title')
-const card = document.querySelector('.card')
 const montant = document.getElementById('montant')
 const equivalance = document.getElementById('equivalance')
 const divEquivalance = document.querySelector('.equivalance')
 const montantRub = document.getElementById('montant-rub')
 const equivalanceRub = document.getElementById('equivalance-rub')
 const divEquivalanceRub = document.querySelector('.equivalance-rub')
-
-const montantAc = document.getElementById('montant-ac')
-const equivalanceAc = document.getElementById('equivalance-ac')
-const divEquivalanceAc = document.querySelector('.equivalance-ac')
-const montantRubAc = document.getElementById('montant-rub-ac')
-const equivalanceRubAc = document.getElementById('equivalance-rub-ac')
-const divEquivalanceRubAc = document.querySelector('.equivalance-rub-ac')
 
 // EVENT LISTENER
 montant.addEventListener('input', conversion)
@@ -55,49 +46,3 @@ function conversionRub() {
     divEquivalanceRub.style.display = 'none'
   }
 }
-
-
-// ================= AC ================= //
-let rateAc = 0.095
-let rateRubAc = 10.40
-
-montantAc.addEventListener('input', () => {
-  // Get user input value
-  const userInputValue = montantAc.value
-  // Conersion
-  const tatal = new Intl.NumberFormat().format(userInputValue * rateAc)
-  // Insert to HTML
-  equivalanceAc.innerHTML = `<b>${tatal}</b>`
-
-  // Dynamic show div equivalance
-  if (userInputValue) {
-    divEquivalanceAc.style.display = 'flex'
-  } else {
-    divEquivalanceAc.style.display = 'none'
-  }
-})
-
-montantRubAc.addEventListener('input', () => {
-  // Get user input value
-  const userInputValue = montantRubAc.value
-  // Conersion
-  const tatal = new Intl.NumberFormat().format(userInputValue * rateRubAc)
-  // Insert to HTML
-  equivalanceRubAc.innerHTML = `<b>${tatal}</b>`
-
-  // Dynamic show div equivalance
-  if (userInputValue) {
-    divEquivalanceRubAc.style.display = 'flex'
-  } else {
-    divEquivalanceRubAc.style.display = 'none'
-  }
-})
-
-// ============== Scroll Reveal ============== //
-const sr = ScrollReveal({
-  distance: '90px',
-  duration: 1500,
-})
-
-sr.reveal(`.title`, { origin: 'top', delay: 300 })
-sr.reveal(`.card-1`, { origin: 'left', delay: 500 })
