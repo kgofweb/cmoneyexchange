93b5ae91d433f886180cba39363da64535624756
mervydevmedia <65782957+kgofweb@users.noreply.github.com>
2022-07-31 19:59:21 +0000

    Add files via upload

---
+++ js/acApp.js
@@ -0,0 +1,48 @@
+// DOM
+const montantAc = document.getElementById('montant-ac')
+const equivalanceAc = document.getElementById('equivalance-ac')
+const divEquivalanceAc = document.querySelector('.equivalance-ac')
+const montantRubAc = document.getElementById('montant-rub-ac')
+const equivalanceRubAc = document.getElementById('equivalance-rub-ac')
+const divEquivalanceRubAc = document.querySelector('.equivalance-rub-ac')
+
+// Rate Only AC
+let rateAc = 0.0999
+let rateRubAc = 10.1
+
+montantAc.addEventListener('input', () => {
+  // Get user input value
+  const userInputValue = montantAc.value
+  // Conersion
+  const tatal = new Intl.NumberFormat().format(userInputValue * rateAc)
+  // Insert to HTML
+  equivalanceAc.innerHTML = `
+    <span><i class="me-3 fa-solid fa-coins"></i> ${tatal}</span> <span>RUB</span>
+  `
+  
+
+  // Dynamic show div equivalance
+  if (userInputValue) {
+    divEquivalanceAc.style.display = 'flex'
+  } else {
+    divEquivalanceAc.style.display = 'none'
+  }
+})
+
+montantRubAc.addEventListener('input', () => {
+  // Get user input value
+  const userInputValue = montantRubAc.value
+  // Conersion
+  const tatal = new Intl.NumberFormat().format(userInputValue * rateRubAc)
+  // Insert to HTML
+  equivalanceRubAc.innerHTML = `
+    <span><i class="me-3 fa-solid fa-coins"></i> ${tatal}</span> <span>CFA</span>
+  `
+
+  // Dynamic show div equivalance
+  if (userInputValue) {
+    divEquivalanceRubAc.style.display = 'flex'
+  } else {
+    divEquivalanceRubAc.style.display = 'none'
+  }
+})
--- js/script.js
+++ js/script.js
@@ -8,12 +8,7 @@ const montantRub = document.getElementById('montant-rub')
 const equivalanceRub = document.getElementById('equivalance-rub')
 const divEquivalanceRub = document.querySelector('.equivalance-rub')
 
-// const montantAc = document.getElementById('montant-ac')
-// const equivalanceAc = document.getElementById('equivalance-ac')
-// const divEquivalanceAc = document.querySelector('.equivalance-ac')
-// const montantRubAc = document.getElementById('montant-rub-ac')
-// const equivalanceRubAc = document.getElementById('equivalance-rub-ac')
-// const divEquivalanceRubAc = document.querySelector('.equivalance-rub-ac')
+
 
 // EVENT LISTENER
 montant.addEventListener('input', conversion)
@@ -21,8 +16,8 @@ montantRub.addEventListener('input', conversionRub)
 
 // ================= AO ================= //
 // Init Rate
-let rateXOF = 0.094
-let rateRUB = 9.34579439
+let rateXOF = 0.0875
+let rateRUB = 9.5238
 
 function conversion () {
   // Get user input value
@@ -55,40 +50,3 @@ function conversionRub() {
     divEquivalanceRub.style.display = 'none'
   }
 }
-
-
-// ================= AC ================= //
-// let rateAc = 0.0999
-// let rateRubAc = 10.1
-
-// montantAc.addEventListener('input', () => {
-//   // Get user input value
-//   const userInputValue = montantAc.value
-//   // Conersion
-//   const tatal = new Intl.NumberFormat().format(userInputValue * rateAc)
-//   // Insert to HTML
-//   equivalanceAc.innerHTML = `<b>${tatal}</b>`
-
-//   // Dynamic show div equivalance
-//   if (userInputValue) {
-//     divEquivalanceAc.style.display = 'flex'
-//   } else {
-//     divEquivalanceAc.style.display = 'none'
-//   }
-// })
-
-// montantRubAc.addEventListener('input', () => {
-//   // Get user input value
-//   const userInputValue = montantRubAc.value
-//   // Conersion
-//   const tatal = new Intl.NumberFormat().format(userInputValue * rateRubAc)
-//   // Insert to HTML
-//   equivalanceRubAc.innerHTML = `<b>${tatal}</b>`
-
-//   // Dynamic show div equivalance
-//   if (userInputValue) {
-//     divEquivalanceRubAc.style.display = 'flex'
-//   } else {
-//     divEquivalanceRubAc.style.display = 'none'
-//   }
-// })
