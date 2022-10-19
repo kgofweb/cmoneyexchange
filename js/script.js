e797781cd63412887b5b830d4e5195612182709a
mervydevmedia <65782957+kgofweb@users.noreply.github.com>
2022-10-19 14:18:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ montantRub.addEventListener('input', conversionRub)
 
 // ================= AO ================= //
 // Init Rate
-let rateXOF = 0.0911
+let rateXOF = 0.0903
 
 let rateRUB = 9.615
 
