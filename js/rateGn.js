f8ab7fd6c86c3ef7c4fae1035d3ab3499929ad10
mervydevmedia <65782957+kgofweb@users.noreply.github.com>
2023-05-01 11:37:36 +0300

    Update rateGn.js

--- js/rateGn.js
+++ js/rateGn.js
@@ -7,7 +7,7 @@ const equivalance_rub_guinee= document.getElementById('equivalance-rub-guinee')
 const divEquivalance_rub_guinee = document.querySelector('.equivalance-rub-guinee')
 
 // Rate Only AC
-let guinee_vers_rus = 0.00794898
+let guinee_vers_rus = 0.00802365
 let russie_vers_gui = 98.477
 
 montant_guinee.addEventListener('input', () => {
