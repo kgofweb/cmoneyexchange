fa3f126f9627fbcf78ec029c148ac118e025db91
mervydevmedia <65782957+kgofweb@users.noreply.github.com>
2025-01-30 14:54:42 +0300

    Update acApp.js

--- js/acApp.js
+++ js/acApp.js
@@ -7,7 +7,7 @@ const equivalanceRubAc = document.getElementById('equivalance-rub-ac')
 const divEquivalanceRubAc = document.querySelector('.equivalance-rub-ac')
 
 // Rate Only AC
-let rateAc = 0.144
+let rateAc = 0.142
 let rateRubAc = 6.1
 
 montantAc.addEventListener('input', () => {
